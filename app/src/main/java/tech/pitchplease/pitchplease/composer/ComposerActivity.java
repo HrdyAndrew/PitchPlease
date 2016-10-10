7a670da3206408354fc76fc1b8ee6873c8f6f3dc
Andrew Hrdy <hrdyandrew@gmail.com>
2016-10-09 22:47:46 -0400

    Fundamentals work, lacks a well designed UI.

--- app/src/main/java/tech/pitchplease/pitchplease/composer/ComposerActivity.java
+++ app/src/main/java/tech/pitchplease/pitchplease/composer/ComposerActivity.java
@@ -1,5 +1,6 @@
 package tech.pitchplease.pitchplease.composer;
 
+import android.animation.ObjectAnimator;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -7,11 +8,12 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
@@ -26,13 +28,11 @@ import retrofit.RetrofitError;
 import retrofit.client.Response;
 import tech.pitchplease.pitchplease.R;
 
-public class ComposerActivity extends AppCompatActivity{
+public class ComposerActivity extends AppCompatActivity {
     private Button btnStart;
-    private Button btnOne;
-    private Button btnTwo;
-    private Button btnThree;
-    private Button btnFour;
+    private Button[] btnSelections;
     private ProgressBar barCountdown;
+    private TextView textScore;
 
     private CountDownTimer countDown;
 
@@ -40,42 +40,115 @@ public class ComposerActivity extends AppCompatActivity{
     private SpotifyService spotify;
     private MediaPlayer mPlayer;
 
+    private String correctArtist = "";
+    private int score = 0;
+    private int rounds = 0;
+    private int sessionID = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composer);
 
-        btnStart = (Button)findViewById(R.id.btnStart);
-        btnOne = (Button)findViewById(R.id.btnOne);
-        btnTwo = (Button)findViewById(R.id.btnTwo);
-        btnThree = (Button)findViewById(R.id.btnThree);
-        btnFour = (Button)findViewById(R.id.btnFour);
-        barCountdown = (ProgressBar)findViewById(R.id.barCountdown);
+        btnSelections = new Button[4];
+        btnStart = (Button) findViewById(R.id.btnStart);
+        btnSelections[0] = (Button) findViewById(R.id.btnOne);
+        btnSelections[1] = (Button) findViewById(R.id.btnTwo);
+        btnSelections[2] = (Button) findViewById(R.id.btnThree);
+        btnSelections[3] = (Button) findViewById(R.id.btnFour);
+        barCountdown = (ProgressBar) findViewById(R.id.barCountdown);
+        textScore = (TextView) findViewById(R.id.textScore);
 
-        //countDown = createCountdownTimer(30000, 1000);
-        //composers = resolveComposerMap();
+        SpotifyApi api = new SpotifyApi();
+        mPlayer = new MediaPlayer();
+        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            @Override
+            public void onPrepared(MediaPlayer mp) {
+                if (mp == mPlayer) {
+                    mPlayer.start();
+                }
+            }
+        });
+
+        spotify = api.getService();
+
+        countDown = createCountdownTimer(30000, 1000);
+        composers = resolveComposerMap();
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 renderGameIcons(true);
-                countDown.start();
+                startRound();
             }
         });
 
-        SpotifyApi api = new SpotifyApi();
-        mPlayer = new MediaPlayer();
-        mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        for (final Button btn : btnSelections) {
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (getCorrectArtist().equals(btn.getText())) {
+                        score++;
+                    }
+                    startRound();
+                }
+            });
+        }
+    }
 
-        spotify = api.getService();
+    private void startRound() {
+        animateProgressBar(30);
+        textScore.setText("Score: " + score + "/" + rounds);
+        mPlayer.stop();
+        Object[] keys = composers.keySet().toArray();
+        String composer = (String) keys[(int) (Math.random() * keys.length)];
+        String alt1 = (String) keys[(int) (Math.random() * keys.length)];
+        String alt2 = (String) keys[(int) (Math.random() * keys.length)];
+        String alt3 = (String) keys[(int) (Math.random() * keys.length)];
+
+        while (composer.equals(alt1)) {
+            alt1 = (String) keys[(int) (Math.random() * keys.length)];
+        }
+        while (composer.equals(alt2)) {
+            alt2 = (String) keys[(int) (Math.random() * keys.length)];
+        }
+        while (composer.equals(alt3)) {
+            alt3 = (String) keys[(int) (Math.random() * keys.length)];
+        }
+
+        Button btnComposer = btnSelections[(int) (Math.random() * btnSelections.length)];
+        Button btnAlt1 = btnSelections[(int) (Math.random() * btnSelections.length)];
+        Button btnAlt2 = btnSelections[(int) (Math.random() * btnSelections.length)];
+        Button btnAlt3 = btnSelections[(int) (Math.random() * btnSelections.length)];
+
+        while (btnComposer == btnAlt1) {
+            btnAlt1 = btnSelections[(int) (Math.random() * btnSelections.length)];
+        }
+        while (btnComposer == btnAlt2) {
+            btnAlt2 = btnSelections[(int) (Math.random() * btnSelections.length)];
+        }
+        while (btnComposer == btnAlt3) {
+            btnAlt3 = btnSelections[(int) (Math.random() * btnSelections.length)];
+        }
+
+        btnComposer.setText(composer.replaceAll("_", " "));
+        btnAlt1.setText(alt1.replaceAll("_", " "));
+        btnAlt2.setText(alt2.replaceAll("_", " "));
+        btnAlt3.setText(alt3.replaceAll("_", " "));
+
+        correctArtist = composer.replaceAll("_", " ");
+        playRandomTrackFromArtist(composers.get(composer));
+        countDown.start();
+
+        rounds++;
     }
 
     private void renderGameIcons(boolean bool) {
         btnStart.setVisibility(bool ? View.INVISIBLE : View.VISIBLE);
-        btnOne.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
-        btnTwo.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
-        btnThree.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
-        btnFour.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
+        for (Button btn : btnSelections) {
+            btn.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
+        }
         barCountdown.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
     }
 
@@ -83,50 +156,50 @@ public class ComposerActivity extends AppCompatActivity{
         return new CountDownTimer(milliseconds, tickInterval) {
             @Override
             public void onTick(long millisUntilFinished) {
-                barCountdown.setProgress((int)millisUntilFinished / 1000);
+                int progress = (int)millisUntilFinished/1000;
+                animateProgressBar(progress <=0 ? 30 : progress);
             }
 
             @Override
             public void onFinish() {
-
+                startRound();
             }
         };
+    }
+
+    private void animateProgressBar(int progress) {
+        ObjectAnimator animation = ObjectAnimator.ofInt(barCountdown, "progress", progress);
+        animation.setDuration(500);
+        animation.setInterpolator(new DecelerateInterpolator());
+        animation.start();
     }
 
     private Map<String, String> resolveComposerMap() {
         Map<String, String> composers = new HashMap<>();
 
-        InputStream stream = null;
-        try {
-            stream = getResources().openRawResource(getResources().getIdentifier("ComposerIDs", "raw", getPackageName()));
-            Scanner scanner = new Scanner(stream);
-            while (scanner.hasNextLine()) {
-                composers.put(scanner.nextLine(), scanner.nextLine());
-                scanner.nextLine();
-            }
-        } finally {
-            if (stream != null) {
-                try {
-                    stream.close();
-                } catch (IOException e) {
-                    //TODO Do something.
-                }
-            }
+        Scanner scanner = new Scanner(getResources().getString(R.string.composers));
+        while (scanner.hasNext()) {
+            composers.put(scanner.next(), scanner.next());
         }
 
         return composers;
     }
 
-    private void playRandomTrackFromArtist(String artistID) {
+    private void playRandomTrackFromArtist(final String artistID) {
         spotify.getArtistAlbums(artistID, new Callback<Pager<Album>>() {
             @Override
             public void success(Pager<Album> albumPager, Response response) {
-                Album album = albumPager.items.get((int)(Math.random() * albumPager.items.size()));
+                Album album = albumPager.items.get((int) (Math.random() * albumPager.items.size()));
                 spotify.getAlbumTracks(album.id, new Callback<Pager<Track>>() {
                     @Override
                     public void success(Pager<Track> trackPager, Response response) {
                         Track track = trackPager.items.get((int) (Math.random() * trackPager.items.size()));
+                        if (track.preview_url == null) {
+                            playRandomTrackFromArtist(artistID);
+                            return;
+                        }
                         try {
+                            mPlayer.setAudioSessionId(++sessionID);
                             mPlayer.setDataSource(getApplicationContext(), Uri.parse(track.preview_url));
                             mPlayer.prepare();
                             mPlayer.start();
@@ -149,5 +222,9 @@ public class ComposerActivity extends AppCompatActivity{
                 System.out.println("FAILURE");
             }
         });
+    }
+
+    private String getCorrectArtist() {
+        return correctArtist;
     }
 }
