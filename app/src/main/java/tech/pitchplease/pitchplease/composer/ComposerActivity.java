f3deece4129a2d4feff6e5a6fb7edd851db323d3
EbMinor3 <evanbelcher3@gmail.com>
2016-10-14 15:08:52 -0400

    -Changed all instances of 'Math.random() * n' with 'Random.nextInt(n)' -Created the newRandomInt() method to generate nonrepeating integers -Made the app release 'mPlayer' on exit of the app (good practice) -Added the answer activity (activity_composer_answer.xml) -Made the startAnswer() method to handle answering the question 	-Made the composer name in the answer link to the composer's wikipedia page 		-Corrected "John Adams" to "John Adams (composer)" when calling the wikipedia page -Generalized the countdown timer length to the variable 'PLAY_TIME'

--- app/src/main/java/tech/pitchplease/pitchplease/composer/ComposerActivity.java
+++ app/src/main/java/tech/pitchplease/pitchplease/composer/ComposerActivity.java
@@ -7,6 +7,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 import android.widget.Button;
@@ -15,7 +17,9 @@ import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
@@ -45,22 +49,34 @@ public class ComposerActivity extends AppCompatActivity {
     private int rounds = 0;
     private int sessionID = 0;
 
+    private final int PLAY_TIME = 30; //play time in seconds
+    private Random rand;
+    private boolean started = false;
+
+    private String composer;
+    private String trackName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        if (!started)
+            super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_composer);
+        rand = new Random();
 
         btnSelections = new Button[4];
-        btnStart = (Button) findViewById(R.id.btnStart);
-        btnSelections[0] = (Button) findViewById(R.id.btnOne);
-        btnSelections[1] = (Button) findViewById(R.id.btnTwo);
-        btnSelections[2] = (Button) findViewById(R.id.btnThree);
-        btnSelections[3] = (Button) findViewById(R.id.btnFour);
-        barCountdown = (ProgressBar) findViewById(R.id.barCountdown);
-        textScore = (TextView) findViewById(R.id.textScore);
+        btnStart = (Button) findViewById(R.id.composerBtnStart);
+        btnSelections[0] = (Button) findViewById(R.id.composerBtnOne);
+        btnSelections[1] = (Button) findViewById(R.id.composerBtnTwo);
+        btnSelections[2] = (Button) findViewById(R.id.composerBtnThree);
+        btnSelections[3] = (Button) findViewById(R.id.composerBtnFour);
+        barCountdown = (ProgressBar) findViewById(R.id.composerBarCountdown);
+        textScore = (TextView) findViewById(R.id.composerTextScore);
 
         SpotifyApi api = new SpotifyApi();
+        if (started && mPlayer != null && mPlayer.isPlaying())
+            mPlayer.stop();
         mPlayer = new MediaPlayer();
+        mPlayer.setScreenOnWhilePlaying(true);
         mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         mPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
             @Override
@@ -73,12 +89,13 @@ public class ComposerActivity extends AppCompatActivity {
 
         spotify = api.getService();
 
-        countDown = createCountdownTimer(30000, 1000);
+        countDown = createCountdownTimer(PLAY_TIME * 1000, 1000);
         composers = resolveComposerMap();
 
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                started = true;
                 renderGameIcons(true);
                 startRound();
             }
@@ -88,50 +105,71 @@ public class ComposerActivity extends AppCompatActivity {
             btn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if (getCorrectArtist().equals(btn.getText())) {
-                        score++;
-                    }
-                    startRound();
+                    renderGameIcons(false);
+                    if (getCorrectArtist().equals(btn.getText()))
+                        startAnswer(true);
+                    else
+                        startAnswer(false);
                 }
             });
         }
+
+        if (started) {
+            renderGameIcons(true);
+            mPlayer.stop();
+            startRound();
+        }
+    }
+
+    private void startAnswer(boolean correct) {
+        countDown.cancel();
+        if (correct)
+            score++;
+        textScore.setText("Score: " + score + "/" + rounds);
+        setContentView(R.layout.activity_composer_answer);
+        TextView answerTitle = (TextView) findViewById(R.id.composerAnswerTitle);
+        TextView answerMessage = (TextView) findViewById(R.id.composerAnswerMessage);
+        Button nextButton = (Button) findViewById(R.id.composerAnswerNextButton);
+        TextView scoreText = (TextView) findViewById(R.id.composerAnswerScoreText);
+        scoreText.setText("Score: " + score + "/" + rounds);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onCreate(new Bundle());
+            }
+        });
+        if (correct)
+            answerTitle.setText("Correct!");
+        else
+            answerTitle.setText("Incorrect!");
+        if (composer.equals("John_Adams"))
+        answerMessage.setText(Html.fromHtml("The piece is <b>" + trackName + "</b> by " +
+                "<a href=\"http://en.wikipedia.org/w/index.php?title=Special%3ASearch&search=" + (composer.replaceAll("_"," ").equals("John Adams") ? composer.replaceAll("_", " ") + " (composer)": composer.replaceAll("_", " ")) + "\"><b>" + composer.replaceAll("_", " ") + "</b></a>"));
+        answerMessage.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     private void startRound() {
-        barCountdown.setProgress(300000);
-        animateProgressBar(0, 29000);
-        textScore.setText("Score: " + score + "/" + rounds);
         mPlayer.stop();
-        Object[] keys = composers.keySet().toArray();
-        String composer = (String) keys[(int) (Math.random() * keys.length)];
-        String alt1 = (String) keys[(int) (Math.random() * keys.length)];
-        String alt2 = (String) keys[(int) (Math.random() * keys.length)];
-        String alt3 = (String) keys[(int) (Math.random() * keys.length)];
+        barCountdown.setProgress(PLAY_TIME * 10000);
+        textScore.setText("Score: " + score + "/" + rounds);
+        LinkedList<String> keys = new LinkedList<>();
+        keys.addAll(composers.keySet());
+        int random1 = newRandomInt(keys.size());
+        int random2 = newRandomInt(keys.size(), random1);
+        int random3 = newRandomInt(keys.size(), random1, random2);
+        composer = keys.get(random1);
+        String alt1 = keys.get(random2);
+        String alt2 = keys.get(random3);
+        String alt3 = keys.get(newRandomInt(keys.size(), random1, random2, random3));
 
-        while (composer.equals(alt1)) {
-            alt1 = (String) keys[(int) (Math.random() * keys.length)];
-        }
-        while (composer.equals(alt2) || alt1.equals(alt2)) {
-            alt2 = (String) keys[(int) (Math.random() * keys.length)];
-        }
-        while (composer.equals(alt3) || alt1.equals(alt3) || alt2.equals(alt3)) {
-            alt3 = (String) keys[(int) (Math.random() * keys.length)];
-        }
+        random1 = newRandomInt(btnSelections.length);
+        random2 = newRandomInt(btnSelections.length, random1);
+        random3 = newRandomInt(btnSelections.length, random1, random2);
 
-        Button btnComposer = btnSelections[(int) (Math.random() * btnSelections.length)];
-        Button btnAlt1 = btnSelections[(int) (Math.random() * btnSelections.length)];
-        Button btnAlt2 = btnSelections[(int) (Math.random() * btnSelections.length)];
-        Button btnAlt3 = btnSelections[(int) (Math.random() * btnSelections.length)];
-
-        while (btnComposer == btnAlt1) {
-            btnAlt1 = btnSelections[(int) (Math.random() * btnSelections.length)];
-        }
-        while (btnComposer == btnAlt2 || btnAlt1 == btnAlt2) {
-            btnAlt2 = btnSelections[(int) (Math.random() * btnSelections.length)];
-        }
-        while (btnComposer == btnAlt3 || btnAlt1 == btnAlt3 || btnAlt2 == btnAlt3) {
-            btnAlt3 = btnSelections[(int) (Math.random() * btnSelections.length)];
-        }
+        Button btnComposer = btnSelections[random1];
+        Button btnAlt1 = btnSelections[random2];
+        Button btnAlt2 = btnSelections[random3];
+        Button btnAlt3 = btnSelections[newRandomInt(btnSelections.length, random1, random2, random3)];
 
         btnComposer.setText(composer.replaceAll("_", " "));
         btnAlt1.setText(alt1.replaceAll("_", " "));
@@ -140,17 +178,42 @@ public class ComposerActivity extends AppCompatActivity {
 
         correctArtist = composer.replaceAll("_", " ");
         playRandomTrackFromArtist(composers.get(composer));
+        animateProgressBar(0, (PLAY_TIME - 1) * 1000);
         countDown.start();
 
         rounds++;
     }
 
+
+    /**
+     * @param count    The number of possible results
+     * @param previous List of values that have already been generated and cannot be generated
+     * @return An integer between between 0 (inclusive) and 'count' (exclusive) that is not contained in 'previous'
+     * @throws IllegalArgumentException - if the size of 'previous' is greater than or equal to 'count'
+     */
+    private int newRandomInt(int count, int... previous) {
+        if (previous.length >= count)
+            throw new IllegalArgumentException("The size of 'previous' cannot be greater than or equal to 'count'");
+        int num;
+        boolean pass;
+        dowhile:
+        do {
+            num = rand.nextInt(count);
+            pass = false;
+            for (int i : previous)
+                if (num == i)
+                    continue dowhile;
+            pass = true;
+        } while (!pass);
+        return num;
+    }
+
     private void renderGameIcons(boolean bool) {
-        btnStart.setVisibility(bool ? View.GONE : View.GONE);
+        btnStart.setVisibility(View.GONE);
         for (Button btn : btnSelections) {
-            btn.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
+            btn.setVisibility(bool ? View.VISIBLE : View.GONE);
         }
-        barCountdown.setVisibility(bool ? View.VISIBLE : View.INVISIBLE);
+        barCountdown.setVisibility(bool ? View.VISIBLE : View.GONE);
     }
 
     private CountDownTimer createCountdownTimer(long milliseconds, long tickInterval) {
@@ -161,9 +224,21 @@ public class ComposerActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                startRound();
+                startAnswer(false);
             }
         };
+    }
+
+    public void onStop() {
+        try {
+            mPlayer.stop();
+            mPlayer.release();
+            mPlayer = null;
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        super.onStop();
     }
 
     private void animateProgressBar(int progress, int duration) {
@@ -188,11 +263,12 @@ public class ComposerActivity extends AppCompatActivity {
         spotify.getArtistAlbums(artistID, new Callback<Pager<Album>>() {
             @Override
             public void success(Pager<Album> albumPager, Response response) {
-                Album album = albumPager.items.get((int) (Math.random() * albumPager.items.size()));
+                Album album = albumPager.items.get(rand.nextInt(albumPager.items.size()));
                 spotify.getAlbumTracks(album.id, new Callback<Pager<Track>>() {
                     @Override
                     public void success(Pager<Track> trackPager, Response response) {
-                        Track track = trackPager.items.get((int) (Math.random() * trackPager.items.size()));
+                        Track track = trackPager.items.get(rand.nextInt(trackPager.items.size()));
+                        trackName = track.name;
                         if (track.preview_url == null) {
                             playRandomTrackFromArtist(artistID);
                             return;
