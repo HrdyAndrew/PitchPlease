578ba5c64fb6029b7fc11d153456b585aca0e4e8
Andrew Hrdy <hrdyandrew@gmail.com>
2016-10-30 13:40:40 -0400

    Actually plays sounds now. Legendary.

--- app/src/main/java/tech/pitchplease/pitchplease/pitchtraining/PitchTrainingActivity.java
+++ app/src/main/java/tech/pitchplease/pitchplease/pitchtraining/PitchTrainingActivity.java
@@ -1,8 +1,12 @@
 package tech.pitchplease.pitchplease.pitchtraining;
 
 import android.animation.ObjectAnimator;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioTrack;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
@@ -23,11 +27,26 @@ public class PitchTrainingActivity extends AppCompatActivity {
     private Button btnSubmit;
     private CountDownTimer countdown;
 
-    private Pitch correctPitch;
+    private Pitch correctPitch = new Pitch(0, Pitch.PitchName.C);
     private int score = 0;
     private int rounds = 0;
 
     private static final int GUESS_TIME = 30; //In seconds
+
+    private boolean playPitch = false;
+    private final int sampleRate = 8000;
+    private final int numSamples = GUESS_TIME * sampleRate;
+    private final double sample[] = new double[numSamples];
+
+    private final byte generatedSnd[] = new byte[2 * numSamples];
+    private AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+            sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+            AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+            AudioTrack.MODE_STATIC);
+
+
+    Handler handler = new Handler();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,12 +88,53 @@ public class PitchTrainingActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                playPitch = false;
+                audioTrack.stop();
                 if (Pitch.getPitchFromTransposition(noteSeekBar.getProgress()) == correctPitch.getPitchName()) {
                     score++;
                 }
                 startRound();
             }
         });
+        renderGameIcons(false);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (playPitch) {
+            playPitch();
+        }
+    }
+
+    void genTone(){
+        // fill out the array
+        for (int i = 0; i < numSamples; ++i) {
+            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/correctPitch.getFrequency(correctPitch.getOctave())));
+        }
+
+        // convert to 16 bit pcm sound array
+        // assumes the sample buffer is normalised.
+        int idx = 0;
+        for (final double dVal : sample) {
+            // scale to maximum amplitude
+            final short val = (short) ((dVal * 32767));
+            // in 16 bit wav PCM, first byte is the low order byte
+            generatedSnd[idx++] = (byte) (val & 0x00ff);
+            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
+
+        }
+    }
+
+    void playSound(){
+        audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
+                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
+                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
+                AudioTrack.MODE_STATIC);
+
+        audioTrack.write(generatedSnd, 0, generatedSnd.length);
+        audioTrack.play();
     }
 
     /**
@@ -92,6 +152,10 @@ public class PitchTrainingActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
+                playPitch = false;
+                if (Pitch.getPitchFromTransposition(noteSeekBar.getProgress()) == correctPitch.getPitchName()) {
+                    score++;
+                }
                 startRound();
             }
         };
@@ -99,15 +163,33 @@ public class PitchTrainingActivity extends AppCompatActivity {
 
     private void startRound() {
         barCountdown.setProgress(GUESS_TIME * 10000);
-        animateProgressBar(0, (GUESS_TIME - 1) * 10000);
+        animateProgressBar(0, (GUESS_TIME - 1) * 1000);
         textScore.setText("Score: " + score + "/" + rounds);
-        correctPitch = playRandomPitch();
+        playRandomPitch();
+        countdown.start();
         rounds++;
     }
 
-    private Pitch playRandomPitch() {
-        //TODO Actually play something..
-        return new Pitch(0, Pitch.PitchName.C);
+    private void playRandomPitch() {
+        correctPitch = new Pitch(4, Pitch.getPitchFromTransposition((int) (Math.random() * 12)));
+        playPitch = true;
+        playPitch();
+    }
+
+    private void playPitch() {
+        // Use a new tread as this can take a while
+        final Thread thread = new Thread(new Runnable() {
+            public void run() {
+                genTone();
+                handler.post(new Runnable() {
+
+                    public void run() {
+                        playSound();
+                    }
+                });
+            }
+        });
+        thread.start();
     }
 
     /**
